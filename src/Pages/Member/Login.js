8e41b90f69675c88a758279b470dbe7db1d7cae7
leeyosi <dark235612@gmail.com>
2020-07-22 10:46:17 +0900

    [이윤식]Refactor: 로그인페이지 1차 리팩토링 - scss 팀 컨벤션 맞춤

--- src/Pages/Member/Login.js
+++ src/Pages/Member/Login.js
@@ -7,25 +7,28 @@ class Login extends Component {
     this.state = {
       email: "",
       pw: "",
+      id: "1",
+      username: "Bret",
       displayactive: false,
       btnactive: false,
     };
   }
 
-  // handlelogin = () => {
-  //   fetch("https://jsonplaceholder.typicode.com/todos/1", {
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       email: this.state.email,
-  //       password: this.state.pw,
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       localStorage.setItem("access_token", res.access_token);
-  //       this.props.history.push("/");
-  //     });
-  // };
+  handlelogin = (e) => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1", {
+      method: "POST",
+      body: JSON.stringify({
+        email: this.state.id,
+        pw: this.state.pw,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        localStorage.setItem("access_token", res.access_token);
+        this.props.history.push("/");
+        console.log(localStorage);
+      });
+  };
   displayhandler = () => {
     this.setState({
       displayactive: !this.state.displayactive,
@@ -55,81 +58,102 @@ class Login extends Component {
     return (
       <div className="Login">
         <img
-          className="Login-logo"
+          className="img_login-logo"
           src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/WiselyLogo.svg"
         />
-        <div className="LoginContainer">
-          <div className="LoginContents">
+        <div className="div_login-container">
+          <div className="div_login-contents">
             <p>
-              <strong className="font-bold font-size">
+              <strong className="div_font-bold div_font-size">
                 로그인 및 회원가입
               </strong>
-              <span className="font-bold-light font-size">을</span>
-              <div className="font-bold-light font-size">시작합니다</div>
+              <span className="div_font-bold-light div_font-size">을</span>
+              <div className="div_font-bold-light div_font-size">
+                시작합니다
+              </div>
             </p>
-            <div className="kakaoSignContainer signContainer">
+            <div className="div_kakaosign-container div_sign-container">
               <img
-                className="loginIcon"
+                className="div_login-icon"
                 src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/signIn/signInKakao.png"
               />
-              <div className="KakaoText font-bold-light">
-                카카오로 <span className="font-bold">간편시작</span>
+              <div className="div_kakao-text div_font-bold-light">
+                카카오로 <span className="div_font-bold">간편시작</span>
               </div>
-              <div className="iconRight" />
+              <div className="div_icon-right" />
             </div>
             <div
-              className="emailSignContainer signContainer"
+              className="div_emailSign-container div_sign-container"
               onClick={this.displayhandler}
             >
               <img
-                className="loginIcon"
+                className="div_login-icon"
                 src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/signIn/signInEmail.png"
               />
-              <div className="signText font-bold-light">이메일로 시작하기</div>
+              <div className="div_signText div_font-bold-light">
+                이메일로 시작하기
+              </div>
               <img
-                className={displayactive ? "iconRight" : "icon-transform"}
+                className={
+                  displayactive ? "div_icon-right" : "div_icon-transform"
+                }
                 src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/arrow/arrowDownPurple.svg"
               />
             </div>
           </div>
-          <div className={displayactive ? "email-box" : "email-box-visible"}>
+          <div
+            className={
+              displayactive ? "div_email-box" : "div_email-box-visible"
+            }
+          >
             <div
-              className={emails ? "div_input-email_blue" : "div_input-email"}
+              className={
+                emails
+                  ? "div_input-email-container-blue"
+                  : "div_input-email-container"
+              }
             >
               <input
                 type="text"
                 placeholder="이메일"
-                className="input-email"
+                className="div_input_email"
                 onChange={this.EmailInput}
               />
               <img
-                className={emails ? "Check-visible" : "Check"}
+                className={emails ? "img_check-visible" : "img_check"}
                 src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/checkBlue.svg"
               />
             </div>
             <div
               className={
-                !this.state.btnactive ? "div_input-pw_visible" : "div_input-pw"
+                !this.state.btnactive
+                  ? "div_input-pw-container-visible"
+                  : "div_input-pw-container"
               }
             >
               <input
                 type="text"
                 placeholder="비밀번호 (6자 이상)"
-                className="input-pw"
+                className="div_input-pw"
                 onChange={this.PwInput}
               />
               <img
-                className={pwd ? "Check-visible" : "Check"}
+                className={pwd ? "img_check-visible" : "img_check"}
                 src="https://wiselyshave-cdn.s3.amazonaws.com/assets/images/checkBlue.svg"
               />
             </div>
             <button
-              className={emails ? "div_btn-Next_blue" : "div_btn-Next"}
+              className={
+                emails
+                  ? "div_btn-Next_container-blue"
+                  : "div_btn-Next-container"
+              }
               onClick={this.btnhandler}
+              onClick={this.handlelogin}
             >
-              <p className="btn-Next">다음</p>
+              <p className="div_btn-Next">다음</p>
             </button>
-            <div className="fg-email">이메일 주소가 기억나지 않아요 </div>
+            <div className="div_fg-email">이메일 주소가 기억나지 않아요 </div>
           </div>
         </div>
       </div>
